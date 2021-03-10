92d79b872ed461b7afc385726701689ae0fd607e
TiberiusBR <felipecs_98@hotmail.com>
2021-03-10 16:45:02 -0300

    Testing trees

--- frontend/src/pages/trees.js
+++ frontend/src/pages/trees.js
@@ -1,19 +1,74 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { AppBar, Toolbar, Container, Button, Card, CardActions, CardContent } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Container,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+} from "@material-ui/core";
 import axios from "axios";
 
+function Comp2() {
+  let user = "Heitor";
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Container style={{ display: "flex", justifyContent: "space-between" }}>
+          <p>Bem vindo, {user}</p>
+          <a href="/new-tree" id="criar-tree">
+            Criar Tree
+          </a>
+        </Container>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+function TreesG(props) {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <h1>Ok bro</h1>
+      {/* {props.trees.map((tree, index) => {
+        return (
+          <Card
+            style={{
+              minWidth: "10%",
+              width: "fit-content",
+              marginTop: "20px",
+              marginLeft: "20px",
+            }}
+          >
+            <CardContent>
+              <h4 style={{ textAlign: "center" }}>{tree.nome}</h4>
+            </CardContent>
+            <CardActions>
+              <Button size="small" href={"/tree/" + tree.nome}>
+                Editar
+              </Button>
+              <Button size="small" >
+                Deletar
+              </Button>
+            </CardActions>
+          </Card>
+        ); */}
+      })}
+    </div>
+  );
+}
+
 export default function Trees() {
-  
   const [email, setEmail] = useState();
   const [trees, setTrees] = useState();
 
-  setEmail(useParams().userEmail);
+  const emailTest = useParams().userEmail
 
   useEffect(async () => {
     try {
       var res = await axios.get(
-        "http://localhost:3333/user/user-trees?email=" + email
+        "http://localhost:3333/user/user-trees?email=" + emailTest
       );
       setTrees(res.data);
     } catch (error) {
@@ -21,78 +76,20 @@ export default function Trees() {
     }
   }, [email]);
 
-  var deleteTree = async () =>{
+  var deleteTree = async () => {
     try {
-        await axios.post("http://localhost:3333/tree/delete" , {userEmail: email})
+      await axios.post("http://localhost:3333/tree/delete", {
+        userEmail: email,
+      });
     } catch (error) {
-        alert("TREE NOT FOUND.")
+      alert("TREE NOT FOUND.");
     }
-}
-
-  function Comp2() {
-    let user = "Heitor";
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          <Container
-            style={{ display: "flex", justifyContent: "space-between" }}
-          >
-            <p>Bem vindo, {user}</p>
-            <a href="/new-tree" id="criar-tree">
-              Criar Tree
-            </a>
-          </Container>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-
-  function TreesG(props) {
-    return (
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {props.trees.map((tree, index) => {
-          return (
-            <Card
-              style={{
-                minWidth: "10%",
-                width: "fit-content",
-                marginTop: "20px",
-                marginLeft: "20px",
-              }}
-            >
-              <CardContent>
-                <h4 style={{ textAlign: "center" }}>{tree.nome}</h4>
-              </CardContent>
-              <CardActions>
-                <Button size="small" href={"/tree/" + tree.nomeUnico}>
-                  Editar
-                </Button>
-                <Button size="small" onClick={deleteTree()}>
-                  Deletar
-                </Button>
-              </CardActions>
-            </Card>
-          );
-        })}
-      </div>
-    );
-  }
-
-  // function PaginaInteira() {
-  //   let a = { nomeUnico: "shitposts", img: "maxresdefault.jpg", nome: "sht pst" }
-  //   let trees = [a, a, a, a, a, a, a, a, a, a, a, a]
-  //   return (
-  //     <div>
-  //       <Comp2 />
-  //       <TreesG trees={trees}/>
-  //     </div>
-  //   )
-  // }
+  };
 
   return (
     <div>
       <Comp2 />
-      <TreesG trees={trees}/>
+      <TreesG trees={trees} />
     </div>
-  )
+  );
 }
